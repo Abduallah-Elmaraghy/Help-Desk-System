666868844d317cea0f352640bc4d62d430e25176
Mi8aelmasry <abdu.mara8y@gmail.com>
2021-05-09 17:55:09 +0200

    Add Home page interface

---
+++ admin/Homepage.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="..\style sheets\stylesheet.css">
+	</head>
+	<body>
+		<?php 
+		include "../includes/nav.php";
+		?>
+		<h3 style="margin-left: 100px;">Pending Cases</h3>
+		<div class="pending_cases">
+			<div class="triangle-left"></div>
+			<div class="pending">
+			</div>
+			<div class="pending">
+			</div>
+			<div class="pending">
+			</div>
+			<div class="triangle-right"></div>
+		</div>
+		<div class="body">
+			<div class="recent_cases">
+
+			</div>
+			<div class="last_Accessed">
+
+			</div>
+		</div>
+	</body>
+</html>
