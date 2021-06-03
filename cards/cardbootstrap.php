fd54fb6e3754cbbc0df97c395e49e809a3cbb393
Mi8aelmasry <abdu.mara8y@gmail.com>
2021-06-04 01:07:38 +0200

    cards and table bootstrap

---
+++ cards/cardbootstrap.php
@@ -0,0 +1,44 @@
+<html>
+	<head>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+	</head>
+	<body>
+		<div class="row row-cols-1 row-cols-md-3 g-4">
+			<div class="col" >
+				<div class="card h-100">
+					<a href="#">
+						<img src="../last%20accessed.png" class="card-img-top" alt="...">
+					</a>
+					<div class="card-body" style="background-color:#1B1717; color: white;">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card h-100">
+					<a href="#">
+						<img src="../last%20accessed.png" class="card-img-top" alt="...">
+					</a>
+					<div class="card-body" style="background-color:#1B1717; color: white;">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">This is a short card.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card h-100">
+					<a href="#">
+						<img src="../last%20accessed.png" class="card-img-top" alt="...">
+					</a>	
+					<div class="card-body" style="background-color:#1B1717; color: white;">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
