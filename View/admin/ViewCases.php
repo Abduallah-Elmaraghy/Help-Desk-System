2d046b8d1761ed6c9ec6dbcba5a64dd91a09ce23
mohamedwahba129 <mohamed1813535@miuegypt.edu.eg>
2021-06-07 00:23:49 +0200

    Viewcases and homepage

--- View/admin/ViewCases.php
+++ View/admin/ViewCases.php
@@ -1,7 +1,114 @@
 <!DOCTYPE html>
 <html>
-	<head> 
-		<style>
+
+<head>
+    <link rel="stylesheet" href="..\style sheets\stylesheet.css">
+</head>
+
+<body>
+    <?php include '../includes/navAdmin.php';?>
+    <select>
+        <option>Type of problem</option>
+        <option>Desktop Application</option>
+        <option>Network</option>
+        <option>Other</option>
+
+    </select>
+    <select>
+        <option>Case Status</option>
+        <option>Pending</option>
+        <option>Opened</option>
+        <option>Resolved</option>
+        <option>Closed</option>
+    </select>
+
+    <input type="date" value="date modified">
+
+    <div class="row">
+	<div class="column">
+            <div class="card">
+                <div class="card_body">
+                    <h4>Case number :</h4>
+                    <h4>Status :</h4>
+                    <input type="submit" value="Open">
+                </div>
+            </div>
+        </div><div class="column">
+            <div class="card">
+                <div class="card_body">
+                    <h4>Case number :</h4>
+                    <h4>Status :</h4>
+                    <input type="submit" value="Open">
+                </div>
+            </div>
+        </div><div class="column">
+            <div class="card">
+                <div class="card_body">
+                    <h4>Case number :</h4>
+                    <h4>Status :</h4>
+                    <input type="submit" value="Open">
+                </div>
+            </div>
+        </div><div class="column">
+            <div class="card">
+                <div class="card_body">
+                    <h4>Case number :</h4>
+                    <h4>Status :</h4>
+                    <input type="submit" value="Open">
+                </div>
+            </div>
+        </div><div class="column">
+            <div class="card">
+                <div class="card_body">
+                    <h4>Case number :</h4>
+                    <h4>Status :</h4>
+                    <input type="submit" value="Open">
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <div class="card_body">
+                    <h4>Case number :</h4>
+                    <h4>Status :</h4>
+                    <input type="submit" value="Open">
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <div class="card_body">
+                    <h4>Case number :</h4>
+                    <h4>Status :</h4>
+                    <input type="submit" value="Open">
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <div class="card_body">
+                    <h4>Case number :</h4>
+                    <h4>Status :</h4>
+                    <input type="submit" value="Open">
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <div class="card_body">
+                    <h4>Case number :</h4>
+                    <h4>Status :</h4>
+                    <input type="submit" value="Open">
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
+<!-- <style>
 			@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap');
 
 
@@ -95,30 +202,4 @@
 				border: none;
 				background-color: #adadad;
 			}
-		</style>
-	</head>
-	<body>
-		<?php include '../includes/navAdmin.php';?>
-		<select>
-			<option >Type of problem</option>
-			<option >Desktop Application</option>
-			<option >Network</option>
-			<option >Other</option>
-
-		</select>
-		<select>
-			<option>Case Status</option>
-			<option>Pending</option>
-			<option>Opened</option>
-			<option>Resolved</option>
-			<option>Closed</option>
-		</select>
-		
-		<input type="date" value="date modified">
-		<!-- <div class="center">  </div>-->
-		<div class="center"></div>
-		<div class="center"></div>
-		<div class="center"></div>
-		<div class="center"> </div>
-	</body>
-</html>
+		</style> -->
