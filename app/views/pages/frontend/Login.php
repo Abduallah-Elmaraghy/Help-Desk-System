1e16718219d206ac5726b285f711e20b4d102577
mohamedwahba129 <mohamed1813535@miuegypt.edu.eg>
2021-06-16 05:19:30 +0200

    Update login.php

--- app/views/pages/frontend/Login.php
+++ app/views/pages/frontend/Login.php
@@ -1,41 +1,38 @@
-<!DOCTYPE html>
-<html lang="eng" dir = "ltr"> 
-	<head>
-		<meta charset="utf-8">
-		<title>Login Page</title>
-		<link rel = "stylesheet" href = "../style sheets/Loginpagestyle.css">
-	</head>
-	<body>
-		<div class = "center" >
-			<h1>Login</h1>
-			<form method="post" action="">
-				<div class="txt_field">
-					<input type="text" name="mail" required>
-					<span> </span>
-					<label>Email</label>
-				</div>
-				<div class="txt_field">
-					<input type="password" name="pass" required>
-					<span> </span>
-					<label>Password</label>
-				</div>
-				<div class="pass" >
-					Forgot Password ?
-				</div>
-				<input type="submit" name = "Login" value = "Login">
-			</form>
-		</div>
-	</body>
-</html>
-<?php 
-//require_once("../Model/classes/person.php");
-if(isset($_POST['Login'])){
-	$p=new Person($_POST['mail'],$_POST['pass']);
-	$p->login($_POST['mail'],$_POST['pass']);
-	/* require_once "../Model/DB/DBconnect.php";
-    $dep = "HEAD";
-    $obj = new DB();
-    $obj->insertrecords($dep);
-    echo "done";*/
+<?php
+class Login extends view
+{
+
+  public function output()
+  {
+    //$title = $this->model->title;
+   // $data = $this->model->data;
+$action= URLROOT . 'public/users/login';
+//$css= APPROOT . 'public/style sheets/Loginpagestyle.css';
+    require APPROOT.'/views/inc/header.php';
+   
+      $text = <<<EOT
+ 
+   <div class = "center" >
+            <h1>Login</h1>
+            <form method="post" action="$action">
+                <div class="txt_field">
+                    <input type="text" name="mail" required>
+                    <span> </span>
+                    <label>Email</label>
+                </div>
+                <div class="txt_field">
+                    <input type="password" name="pass" required>
+                    <span> </span>
+                    <label>Password</label>
+                </div>
+                <div class="pass" >
+                    Forgot Password ?
+                </div>
+                <input type="submit" name = "Login" value = "Login">
+            </form>
+        </div>
+EOT;
+    echo $text;
+ 
+  }
 }
-?>
